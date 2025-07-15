bf8bc5a81ff601abd9d7b407280a423c8c5e0d1d
jvznx07 <joaoaprigio10303@gmail.com>
2025-07-15 09:49:53 -0300

    Add files via upload

--- sudoku/verificadores.py
+++ sudoku/verificadores.py
@@ -1,5 +1,21 @@
 # Funções a serem implementadas futuramente:
 def verifica_colunas(pos_preenchidas):
+    # Percorre cada coluna
+    for coluna in range(9):
+        # uma lista não ordenada de valores vistos(conjunto)
+        val_vistos = set()
+        # vai percorrer todo o pos_preenchida e o item assume o valor da lista inserida no pos_preenchidas
+        for item in pos_preenchidas:
+            # verifica se o número da coluna corresponde ao atual verificado no laço
+            if item[0] == coluna:
+                valor = item[2]
+                # se o valor já estiver sido adicionado, a função retorna False
+                if valor in val_vistos:
+                    return False
+                
+                # Adiciona o valor ao conjunto val_vistos
+                val_vistos.add(valor)    
+    
     """Verifica se uma coluna não tem números repetidos."""
     return True
 
@@ -26,9 +42,25 @@ def verifica_linhas(pos_preenchidas):
 
 
 def verifica_quadrante(pos_preenchidas):
+    inicio_linha = (pos_preenchidas[1] // 3) * 3
+    inicio_coluna = (pos_preenchidas[0] // 3) * 3
+
+    for i in range(inicio_linha, inicio_linha + 3):
+        for j in range(inicio_coluna, inicio_coluna + 3):
+            if pos_preenchidas[i][j] == pos_preenchidas[3]:
+                return True
+
+    
+    
+    
+    
+    
+    
+    
+    
     """Verifica se um quadrante 3x3 não tem números repetidos."""
 
-    return True
+    return False
 
 
 def val_possiveis(pos_check, pos_preenchidas):
